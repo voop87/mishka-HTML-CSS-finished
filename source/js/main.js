7dd78626167142ebe9d54b4925ae0196bd81ab10
Ruslan Mansurov <mansurov87@gmail.com>
2020-02-11 17:58:13 +0300

    Добавляет js код появление меню и модального окна

---
+++ source/js/main.js
@@ -0,0 +1,24 @@
+//Показать главное меню
+var closeMenuBtn = document.getElementById("page-header__menu-toggle");
+closeMenuBtn.classList.add("page-header__menu-toggle--js");
+
+var showMenu = function() {
+  var mainNav = document.getElementById("main-nav");
+  var searchAndCartMenu = document.getElementById("search-and-cart");
+
+  mainNav.classList.toggle("main-nav--opened");
+  searchAndCartMenu.classList.toggle("search-and-cart--opened");
+};
+
+//Показать модальное окно
+var showModal = function() {
+  var modal = document.getElementById("modal");
+
+  modal.style.display = "block";
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+};
